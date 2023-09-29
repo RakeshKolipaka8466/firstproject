9f837b28d59739917c799e367229f4b1267eb01d
Rakesh <rakeshkolipka8466@gmail.com>
2023-09-29 20:31:02 +0530

    scope

---
+++ assets/JSPgms/scope.js
@@ -0,0 +1,17 @@
+// var a=40;
+// function call(){
+//     a=60;
+//     console.log(a);//60
+// }
+// call();
+
+
+var a=40;
+function call(){
+    a=60;
+    let b=500;
+    console.log(a);
+}
+call();
+console.log(b);
+
