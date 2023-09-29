ce4b7421f622fedb84191a974c4687efc1a1770b
Rakesh <rakeshkolipka8466@gmail.com>
2023-09-29 22:56:54 +0530

    modified functionalscope

--- assets/JSPgms/scope.js
+++ assets/JSPgms/scope.js
@@ -18,15 +18,44 @@
 
 
 //3 types of scope global scope,functional scope,block scope
-if(true){
-    var firstName="Vishal";
-    let middleName="Sharma";
-    const lastName="Sir";
-    console.log(firstName);
-    console.log(middleName);
-    console.log(lastName);
+//Block Scope
+// if(true){
+//     var firstName="Vishal"; //firstName is global Scope
+//     let middleName="Sharma";//middleName and LastName is the blockScope
+//     const lastName="Sir";
+//     console.log(firstName);//Vishal
+//     console.log(middleName);//Sharma
+//     console.log(lastName);//Sir
+// }
+// console.log(firstName);//Vishal
+// console.log(middleName);//uncaught reference error middleName is not defined
+// console.log(lastName);
+
+
+
+//Functional Scope
+var a=40;
+function varScope(){
+    var a=60;
+    var c=60;
+    let s=30;
+    const ss=60;
+    console.log("a",a);//a 60
+    console.log("c",c);//c 60
+    console.log("s",s);//s 30
+    console.log("ss",ss);//ss 60
 }
-console.log(firstName);
-console.log(middleName);
-console.log(lastName);
+varScope();
+console.log("outside===");//outside===
+console.log("a",a);//a 40
+console.log("c",c);//uncaught reference error c is not defined
+console.log("s",s);
+console.log("ss",ss);
+
+
+
+
+
+
+
 
