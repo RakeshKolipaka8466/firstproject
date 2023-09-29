e369c50ba84c659a97b92481b2888a5e519f3fd8
Rakesh <rakeshkolipka8466@gmail.com>
2023-09-29 21:39:21 +0530

    modified blockscope

--- assets/JSPgms/scope.js
+++ assets/JSPgms/scope.js
@@ -6,12 +6,27 @@
 // call();
 
 
-var a=40;
-function call(){
-    a=60;
-    let b=500;
-    console.log(a);
+// var a=40;
+// function call(){       //total golbalscope
+//     a=60;              //call(){} //its lexicalscope(its own scope + parent scope) r local scope
+//     let b=500;
+//     console.log(a);//60
+// }
+// call();
+// console.log(b);//uncaught reference error b is not defined
+
+
+
+//3 types of scope global scope,functional scope,block scope
+if(true){
+    var firstName="Vishal";
+    let middleName="Sharma";
+    const lastName="Sir";
+    console.log(firstName);
+    console.log(middleName);
+    console.log(lastName);
 }
-call();
-console.log(b);
+console.log(firstName);
+console.log(middleName);
+console.log(lastName);
 
