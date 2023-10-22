bc730bfb1538468a0dce3b78021c711ac7974028
Rakesh <rakeshkolipka8466@gmail.com>
2023-10-23 02:30:04 +0530

    DOM

---
+++ assets/JSPgms/dom.js
@@ -0,0 +1,9 @@
+console.log("DOM Material");
+const rootE=document.getElementById("root");
+console.log(rootE);
+rootE.innerText="Hey DOM Material";
+rootE.style.fontSize="36px";
+rootE.style.color="red";
+rootE.style.margin="auto";
+rootE.style.width="100%";
+rootE.style.textAlign="center";
